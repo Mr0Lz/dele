bac3d359f9f18ae36bc7d193cd6cdc4ab6de9881
lz <lz>
2018-09-12 14:11:05 +0800

    dev添加自动打开浏览器

--- config/index.js
+++ config/index.js
@@ -3,6 +3,23 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
+const os=require('os');
+var interfaces=os.networkInterfaces();
+var ip=null;
+//webpack-dev-server  配置autoOpenBrowser=true,server的open=true,
+//根据host配置自动打开浏览器,host:0.0.0.0 局域网和本地使用locahost,127.0.0.1能访问,
+//打开浏览器是根据host:0.0.0.0所以本地是访问不了
+//解决 1.自动获取电脑ip  能在本地和局域网内打开
+//2.用opn  自己写打开浏览器的功能
+
+for(var k in interfaces){
+  interfaces[k].forEach(function (currentValue, index, arr) {
+      if(currentValue.family=="IPv4"&&currentValue.mac!='00:00:00:00:00:00'){
+        ip=currentValue.address;
+      }
+    });
+}
+console.log(ip)
 
 module.exports = {
   dev: {
@@ -13,9 +30,10 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    //host: '0.0.0.0', // can be overwritten by process.env.HOST
+    host: ip,
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
