0256a2f0b13629ae62269f2fc2ab7d774def4800
lz <lz>
2018-09-27 17:02:57 +0800

    添加 header footber 今日推荐

--- src/page/recommendedToday.vue
+++ src/page/recommendedToday.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
+        <head-nav></head-nav>
     </div>
 </template>
 <script>
+import headNav from './../components/header/headNav'
+
 export default {
-    
+    name: 'recommendedToday',
+    components: {
+        headNav
+    }
 }
 </script>
 <style lang="scss" scoped>
