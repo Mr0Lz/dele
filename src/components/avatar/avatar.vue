327ed120a6376312c1bfd5b50dce82f1d0d58728
lz <webpagemakerlz@163.com>
2018-10-09 15:36:14 +0800

    添加头像组件

---
+++ src/components/avatar/avatar.vue
@@ -0,0 +1,41 @@
+<template>
+    <section :class=" 'round avatar-'+size ">
+        <img :src="imgSrc ? imgSrc : defaultImg " alt="">
+    </section>
+</template>
+<script>
+
+export default {
+    name: 'avatar',
+    data(){
+        return {
+            defaultImg:"static/avatar.jpg"
+        }
+    },
+    props:{
+        size:String,
+        imgSrc:String
+    }
+}
+</script>
+<style lang="scss" scoped>
+    @import "../../style/mixin";
+    .round{
+        @include borderRadius(50%);
+        overflow: hidden;
+        background: $colCCC;
+        img {
+            @include wh(100%,100%);
+        }
+    }
+    .avatar-s{
+        @include wh(0.3rem,0.3rem);
+    }
+    .avatar-m{
+        @include wh(0.6rem,0.6rem);
+    }
+    .avatar-l{
+        @include wh(1rem,1rem);
+    }
+</style>
+
