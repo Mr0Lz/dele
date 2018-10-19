605ef5317f3fa2fdfc4cbecf0cdf6e33a9f87f08
lz <webpagemakerlz@163.com>
2018-10-19 18:04:27 +0800

    slideDelete(左滑删除)组件开发中

--- src/components/slideDelete/slideDelete.vue
+++ src/components/slideDelete/slideDelete.vue
@@ -1,7 +1,8 @@
 <template>
     
-    <section class="slide-delete-template" @touchstart="touchstart" @touchend="touchend">
+    <section class="slide-delete-template" @touchstart.stop="touchstart"  @touchmove="touchmove"  @touchend.stop="touchend">
         <slot name="slide-container"></slot>
+        <div class="remove-item"><span>删除</span></div>
     </section>
 
 </template>
@@ -11,20 +12,41 @@ export default {
     name:'slideDelete',
     data(){
         return {
-            x:0
+            startX:0,
+            endX:0
         }
     },
     methods:{
-        touchstart($event){
-            console.log('start');
+        touchstart(event){
+           this.startX=event.touches[0].clientX;
         },
-        touchend($event){
-            console.log('end');
+        touchmove(event){
+           this.endX=event.touches[0].clientX;
+        },
+        touchend(event){
+            console.log(this.startX,this.endX,event.target);
+            this.startX=null;
+            this.endX=null;
         }
     }
 }
 </script>
 
-<style>
+<style lang="scss">
+    @import "../../style/mixin";
+
+    .slide-delete-template{
+        position: relative;
+        overflow: hidden;
+        .remove-item{
+            @include absolute(0,100%);
+            @include wh(30%,100%);
+            background: $colE7290F;
+            span{
+                @include center();
+                @include font(0.16rem,$c:$colFFF); 
+            }
+        }
+    }
 
 </style>
--- src/page/shoppingBag.vue
+++ src/page/shoppingBag.vue
@@ -2,18 +2,22 @@
     <section class="shoppingBag page-container">
         <head-nav title="购物袋"></head-nav>
 
-        <section class="shoppingBag-container m-t">
+        <section class="shoppingBag-container ">
 
             <div class="h-1 m-l m-r">购物袋</div>
 
-            <div class="commodity-list">
+            <div class="commodity-list m-t">
 
                 <slide-delete class="commodity-item">
-                    <div slot="slide-container">
+                    <div slot="slide-container" class="m-l">
                         asda
                     </div>
                 </slide-delete>
-
+                <slide-delete class="commodity-item">
+                    <div slot="slide-container" class="m-l">
+                        asda
+                    </div>
+                </slide-delete>
             </div>
 
         </section>
@@ -41,6 +45,17 @@ export default {
     @import '../style/mixin';
 
     .shoppingBag{
-        background: $colF2
+        background: $colF2;
+        .shoppingBag-container{
+            background: $colFFF;
+            padding-top:0.3rem; 
+            .commodity-list{
+                .commodity-item{
+                    height: 1rem;
+                    background: $col3791FA;
+                    font-size: 0.12rem;
+                }
+            }
+        }
     }
 </style>
