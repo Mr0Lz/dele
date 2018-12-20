6e94b1406aed77a38aad50e9f06d83036fb576ae
lz <webpagemakerlz@163.com>
2018-12-20 17:56:29 +0800

    webpack中利用require.ensure()实现按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import today from '@/page/today/today' //首页,今日推荐页面
-import personal from '@/page/personal/personal' //个人中心页面
-import shoppingBag from '@/page/shoppingBag/shoppingBag' //购物袋页面
-import productDetails from '@/page/productDetails/productDetails'//产品详情页面
+//import today from '@/page/today/today' //首页,今日推荐页面
+//import personal from '@/page/personal/personal' //个人中心页面
+//import shoppingBag from '@/page/shoppingBag/shoppingBag' //购物袋页面
+//import productDetails from '@/page/productDetails/productDetails'//产品详情页面
+//webpack中利用require.ensure()实现按需加载
+const today = r => require.ensure([],() => r(require('@/page/today/today')),'today')
+const personal = r => require.ensure([],() => r(require('@/page/personal/personal')),'personal')
+const shoppingBag = r => require.ensure([],() => r(require('@/page/shoppingBag/shoppingBag')),'shoppingBag')
+const productDetails = r => require.ensure([],() => r(require('@/page/productDetails/productDetails')),'productDetails')
+
 
 Vue.use(Router)
 
