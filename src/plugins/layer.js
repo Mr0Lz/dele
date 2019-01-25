702bb5bc27ead960e24e1fc4fb49f8c28bf8025b
lz <webpagemaker@163.com>
2019-01-25 17:28:31 +0800

    研究同一台电脑2个账号

--- src/plugins/layer.js
+++ src/plugins/layer.js
@@ -4,5 +4,5 @@ function Layer() {
     //     this.time= !opt.time ? 0 : opt.time > 0 ? opt.time : 2000 ;//某毫秒内自动关闭弹框 0或者false不开启
     //     this.content='样式完全自定义的layer';
     // }
-    //研究模块化编程
+    //研究模块化编程   研究同一台电脑2个账号
 }
